d81e929bf0f0df63d65d8634accc63e4145192b7
Ivan Tinoco <tinoco.ivan@gmail.com>
2017-05-02 10:28:22 -0700

    First Commit

---
+++ CAR/CAR.ino
@@ -0,0 +1,141 @@
+/*  ME106 Term Project
+*  SJSU - Spring 2017
+*  Hand Gesture Controlled Robotic Car*/
+
+//CAR-MODULE
+//Code to control DC-Motors and RF Receiver
+
+//DAC/ADC Library used for RF Transmission
+#include <RH_ASK.h>   // (Uses pin 11 to read data)
+#include <SPI.h>      //Not actually used but needed to compile (RH_ASK.h Library specified)
+
+RH_ASK driver;        
+
+//DC-motor driver control pins:
+int in1=9;
+int in2=8;
+int in3=7;
+int in4=6;
+int ENA=5;
+int ENB=3;
+int ABS = 130;
+
+//TX Control counter
+int count = 0;
+
+//Car-movement control functions:
+void _mForward()
+{ 
+digitalWrite(ENA,ABS);
+digitalWrite(ENB,ABS);
+digitalWrite(in1,LOW);
+digitalWrite(in2,HIGH);
+digitalWrite(in3,LOW);
+digitalWrite(in4,HIGH);
+Serial.println("forward");
+}
+void _mBack()
+{
+digitalWrite(ENA,ABS);
+digitalWrite(ENB,ABS);
+digitalWrite(in1,HIGH);
+digitalWrite(in2,LOW);
+digitalWrite(in3,HIGH);
+digitalWrite(in4,LOW);
+Serial.println("back");
+}
+void _mleft()
+{
+digitalWrite(ENA,ABS);
+digitalWrite(ENB,ABS);
+digitalWrite(in1,LOW);
+digitalWrite(in2,HIGH);
+digitalWrite(in3,HIGH);
+digitalWrite(in4,LOW);
+Serial.println("left");
+}
+void _mright()
+{
+digitalWrite(ENA,ABS);
+digitalWrite(ENB,ABS);
+digitalWrite(in1,HIGH);
+digitalWrite(in2,LOW);
+digitalWrite(in3,LOW);
+digitalWrite(in4,HIGH);
+//Serial.println("right");
+}
+void _mStop()
+{
+digitalWrite(ENA,LOW);
+digitalWrite(ENB,LOW);
+Serial.println("STOP");
+}
+
+//Initial setup I/O + Serial Comm + RF Comm
+void setup()
+{
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3,OUTPUT);
+  pinMode(in4,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+
+  //Serial Comm use to debug
+  Serial.begin(9600);
+  
+  _mStop();
+
+  //RF module initialize:
+  if (!driver.init()) Serial.println("init failed");
+}
+
+//Continuous signal reading/processing:
+void loop()
+{
+  //RH_ASK.h library ADC processing:
+  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+  uint8_t buflen = sizeof(buf);
+
+  //if RF signal recieved:
+  if (driver.recv(buf, &buflen)) // Non-blocking
+  {
+    count = 0;
+    Serial.println(buf[0]); //debugging
+    if(buf[0]=='0')
+    {
+      _mForward();
+    } 
+    else if(buf[0]=='1')
+    {
+      _mBack();
+    }  
+    else if(buf[0]=='2')
+    {
+      _mright();
+    }
+    else if(buf[0]=='3')
+    {
+      _mleft();
+    }   
+    else if(buf[0]=='4')
+    { 
+      _mStop();
+    }
+    else
+    {
+      _mStop();
+    }
+  }
+  else
+  {
+    count++;
+  }
+
+  Serial.println(count);
+  if (count > 200 ) //Transmission lost
+  {
+    _mStop();
+  }
+} 
+
---
+++ TXControl/TXControl.ino
@@ -0,0 +1,143 @@
+/*  ME106 Term Project
+*  SJSU - Spring 2017
+*  Hand Gesture Controlled Robotic Car*/
+
+//CONTROL-MODULE
+//Code to read MPU6050 (Gyroscope Sensor) data and RF Transmitter
+
+//Libraris used to communicate with MPU6050 module:
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+//DAC/ADC Library used for RF Transmission:
+#include <RH_ASK.h>   // (Uses pin 12 to send data)
+#include <SPI.h>      // Not actually used but needed to compile
+
+RH_ASK driver;
+
+//LED pins used for debugging:
+//#define pinA 4
+//#define pinB 5
+//#define pinC 6
+//#define pinD 7
+
+//Initial setup I/O + Serial Comm + RF Comm
+void setup() 
+{
+  //Debug only LED pins
+//  pinMode(pinA, OUTPUT);
+//  pinMode(pinB, OUTPUT);
+//  pinMode(pinC, OUTPUT);
+//  pinMode(pinD, OUTPUT);
+
+  Serial.begin(9600);
+
+  //RF module initialize:
+  if (!driver.init()) Serial.println("init failed");
+
+  //Initialize MPU6050:
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+}
+
+//Continuous data reading/processing, signal send:
+void loop()
+{
+  //MPU6050 data read
+  Vector rawAccel = mpu.readRawAccel();
+  Vector normAccel = mpu.readNormalizeAccel();
+
+//  Used to read raw values and debugging
+//  Serial.print(" Xraw = ");
+//  Serial.println(rawAccel.XAxis);
+//  Serial.print(" Yraw = ");
+//  Serial.println(rawAccel.YAxis);
+//  Serial.println();
+//  delay(500);
+//  Serial.print(" Zraw = ");
+//  Serial.println(rawAccel.ZAxis);
+
+  if(rawAccel.XAxis < -7000 && rawAccel.YAxis < 5999  && rawAccel.YAxis > -5999) //forward
+  {
+    //LED pins - Debug only
+//    digitalWrite(pinA, LOW);
+//    digitalWrite(pinB, HIGH);
+//    digitalWrite(pinC, HIGH);
+//    digitalWrite(pinD, LOW);
+
+    //Control Signal
+    sendSignal("0");
+  }
+  else if(rawAccel.XAxis > 7000 && rawAccel.YAxis < 5999  && rawAccel.YAxis > -5999) //backward
+  {
+    //LED pins - Debug only
+//    digitalWrite(pinA, HIGH);
+//    digitalWrite(pinB, LOW);
+//    digitalWrite(pinC, LOW);
+//    digitalWrite(pinD, HIGH);
+    
+    //Control Signal
+    sendSignal("1"); 
+  }
+  else if(rawAccel.YAxis > 6000 && rawAccel.XAxis < 6999 && rawAccel.XAxis > -6999)//right
+  {
+    //LED pins - Debug only
+//    digitalWrite(pinA, LOW);
+//    digitalWrite(pinB, LOW);
+//    digitalWrite(pinC, HIGH);
+//    digitalWrite(pinD, HIGH);
+    
+    //Control Signal
+    sendSignal("2");
+  }
+  else if(rawAccel.YAxis < -6000 && rawAccel.XAxis < 5999 && rawAccel.XAxis > -2999)//left
+  {
+    //LED pins - Debug only
+//    digitalWrite(pinA, HIGH);
+//    digitalWrite(pinB, HIGH);
+//    digitalWrite(pinC, LOW);
+//    digitalWrite(pinD, LOW);
+    
+    //Control Signal
+    sendSignal("3");
+  }
+  else if(rawAccel.XAxis < 6999 && rawAccel.XAxis > -6999 && rawAccel.YAxis < 5999  && rawAccel.YAxis > -5999) //stop
+  {
+    //LED pins - Debug only
+//    digitalWrite(pinA, LOW);
+//    digitalWrite(pinB, HIGH);
+//    digitalWrite(pinC, LOW);
+//    digitalWrite(pinD, HIGH);
+    
+    //Control Signal
+    sendSignal("4");
+  }
+  else
+  {
+    //LED pins - Debug only
+//    digitalWrite(pinA, LOW);
+//    digitalWrite(pinB, LOW);
+//    digitalWrite(pinC, LOW);
+//    digitalWrite(pinD, LOW);
+    
+    //Control Signal
+    sendSignal("5");
+  }
+}
+
+//Function to send a signal using RF module
+void sendSignal(const char *controller)
+{
+  Serial.print("Sending => "); //debugging
+  Serial.println(controller); //debugging
+  
+  driver.send((uint8_t *)controller, strlen(controller));
+  driver.waitPacketSent();
+}
+
+
