02ad626a4ae117e0578b6f2df13c821f77161408
Ivan Tinoco <tinoco.ivan@gmail.com>
2017-05-03 14:03:02 -0700

    Fixed sensitivity.

--- TXControl/TXControl.ino
+++ TXControl/TXControl.ino
@@ -1,6 +1,6 @@
 /*  ME106 Term Project
-*  SJSU - Spring 2017
-*  Hand Gesture Controlled Robotic Car*/
+   SJSU - Spring 2017
+   Hand Gesture Controlled Robotic Car*/
 
 //CONTROL-MODULE
 //Code to read MPU6050 (Gyroscope Sensor) data and RF Transmitter
@@ -24,13 +24,13 @@ RH_ASK driver;
 //#define pinD 7
 
 //Initial setup I/O + Serial Comm + RF Comm
-void setup() 
+void setup()
 {
   //Debug only LED pins
-//  pinMode(pinA, OUTPUT);
-//  pinMode(pinB, OUTPUT);
-//  pinMode(pinC, OUTPUT);
-//  pinMode(pinD, OUTPUT);
+  //  pinMode(pinA, OUTPUT);
+  //  pinMode(pinB, OUTPUT);
+  //  pinMode(pinC, OUTPUT);
+  //  pinMode(pinD, OUTPUT);
 
   Serial.begin(9600);
 
@@ -38,7 +38,7 @@ void setup()
   if (!driver.init()) Serial.println("init failed");
 
   //Initialize MPU6050:
-  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
   {
     Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
     delay(500);
@@ -52,79 +52,79 @@ void loop()
   Vector rawAccel = mpu.readRawAccel();
   Vector normAccel = mpu.readNormalizeAccel();
 
-//  Used to read raw values and debugging
-//  Serial.print(" Xraw = ");
-//  Serial.println(rawAccel.XAxis);
-//  Serial.print(" Yraw = ");
-//  Serial.println(rawAccel.YAxis);
-//  Serial.println();
-//  delay(500);
-//  Serial.print(" Zraw = ");
-//  Serial.println(rawAccel.ZAxis);
+  //  Used to read raw values and debugging
+  //  Serial.print(" Xraw = ");
+  //  Serial.println(rawAccel.XAxis);
+  //  Serial.print(" Yraw = ");
+  //  Serial.println(rawAccel.YAxis);
+  //  Serial.println();
+  //  delay(500);
+  //  Serial.print(" Zraw = ");
+  //  Serial.println(rawAccel.ZAxis);
 
-  if(rawAccel.XAxis < -7000 && rawAccel.YAxis < 5999  && rawAccel.YAxis > -5999) //forward
+  if (rawAccel.XAxis < -7000 && rawAccel.XAxis > -14000 && rawAccel.YAxis < 5999  && rawAccel.YAxis > -5999) //forward
   {
     //LED pins - Debug only
-//    digitalWrite(pinA, LOW);
-//    digitalWrite(pinB, HIGH);
-//    digitalWrite(pinC, HIGH);
-//    digitalWrite(pinD, LOW);
+    //    digitalWrite(pinA, LOW);
+    //    digitalWrite(pinB, HIGH);
+    //    digitalWrite(pinC, HIGH);
+    //    digitalWrite(pinD, LOW);
 
     //Control Signal
     sendSignal("0");
   }
-  else if(rawAccel.XAxis > 7000 && rawAccel.YAxis < 5999  && rawAccel.YAxis > -5999) //backward
+  else if (rawAccel.XAxis > 7000 && rawAccel.XAxis < 14000 && rawAccel.YAxis < 5999  && rawAccel.YAxis > -5999) //backward
   {
     //LED pins - Debug only
-//    digitalWrite(pinA, HIGH);
-//    digitalWrite(pinB, LOW);
-//    digitalWrite(pinC, LOW);
-//    digitalWrite(pinD, HIGH);
-    
+    //    digitalWrite(pinA, HIGH);
+    //    digitalWrite(pinB, LOW);
+    //    digitalWrite(pinC, LOW);
+    //    digitalWrite(pinD, HIGH);
+
     //Control Signal
-    sendSignal("1"); 
+    sendSignal("1");
   }
-  else if(rawAccel.YAxis > 6000 && rawAccel.XAxis < 6999 && rawAccel.XAxis > -6999)//right
+  else if (rawAccel.YAxis > 7000 && rawAccel.YAxis < 14000 && rawAccel.XAxis < 6999 && rawAccel.XAxis > -6999) //right
   {
     //LED pins - Debug only
-//    digitalWrite(pinA, LOW);
-//    digitalWrite(pinB, LOW);
-//    digitalWrite(pinC, HIGH);
-//    digitalWrite(pinD, HIGH);
-    
+    //    digitalWrite(pinA, LOW);
+    //    digitalWrite(pinB, LOW);
+    //    digitalWrite(pinC, HIGH);
+    //    digitalWrite(pinD, HIGH);
+
     //Control Signal
     sendSignal("2");
   }
-  else if(rawAccel.YAxis < -6000 && rawAccel.XAxis < 5999 && rawAccel.XAxis > -2999)//left
+  else if (rawAccel.YAxis < -7000 && rawAccel.YAxis > -14000 && rawAccel.XAxis < 5999 && rawAccel.XAxis > -2999) //left
   {
     //LED pins - Debug only
-//    digitalWrite(pinA, HIGH);
-//    digitalWrite(pinB, HIGH);
-//    digitalWrite(pinC, LOW);
-//    digitalWrite(pinD, LOW);
-    
+    //    digitalWrite(pinA, HIGH);
+    //    digitalWrite(pinB, HIGH);
+    //    digitalWrite(pinC, LOW);
+    //    digitalWrite(pinD, LOW);
+
     //Control Signal
     sendSignal("3");
   }
-  else if(rawAccel.XAxis < 6999 && rawAccel.XAxis > -6999 && rawAccel.YAxis < 5999  && rawAccel.YAxis > -5999) //stop
+  else if (rawAccel.XAxis < 6999 && rawAccel.XAxis > -6999 && rawAccel.YAxis < 5999  && rawAccel.YAxis > -5999) //stop
   {
     //LED pins - Debug only
-//    digitalWrite(pinA, LOW);
-//    digitalWrite(pinB, HIGH);
-//    digitalWrite(pinC, LOW);
-//    digitalWrite(pinD, HIGH);
-    
+    //    digitalWrite(pinA, LOW);
+    //    digitalWrite(pinB, HIGH);
+    //    digitalWrite(pinC, LOW);
+    //    digitalWrite(pinD, HIGH);
+
     //Control Signal
     sendSignal("4");
   }
   else
   {
     //LED pins - Debug only
-//    digitalWrite(pinA, LOW);
-//    digitalWrite(pinB, LOW);
-//    digitalWrite(pinC, LOW);
-//    digitalWrite(pinD, LOW);
-    
+    //    digitalWrite(pinA, LOW);
+    //    digitalWrite(pinB, LOW);
+    //    digitalWrite(pinC, LOW);
+    //    digitalWrite(pinD, LOW);
+
     //Control Signal
     sendSignal("5");
   }
@@ -135,7 +135,7 @@ void sendSignal(const char *controller)
 {
   Serial.print("Sending => "); //debugging
   Serial.println(controller); //debugging
-  
+
   driver.send((uint8_t *)controller, strlen(controller));
   driver.waitPacketSent();
 }
